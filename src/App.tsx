fa231f124a43ba0f8e555b16d5d91d0762f4c876
Shiiba1212 <earth.frg@gmail.com>
2024-03-28 16:26:56 +0900

    add todo appli

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { FormEvent, useState } from 'react'
+// import './App.css'
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([
+    // {id:"1",
+    // title:"aaa",
+    // completed:false
+    // },
+    // {id:"2",
+    // title:"ttt",
+    // completed:true
+    // }
+  ]
+  )
+  const [input, setInput] = useState('')
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    const task: Task = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false
+    }
+    setTasks([...tasks,task])
+    setInput('')
+  }
+
+  function handleCheckBoxChange(task: Task): void{
+    setTasks(tasks.map(t =>{
+      if(t.id === task.id)
+        return {
+         ...t,
+         completed: !t.completed
+        }
+        return t        
+    }))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TODO アプリ</h1>
+      {tasks.length > 0 ?
+        <>
+          <h2>My Tasks</h2>
+          <ul>
+            {tasks.map(task =>
+              <li key={task.id}>
+                <input type="checkbox" checked={task.completed}
+                onChange={() => handleCheckBoxChange(task)}></input>
+                {task.title}
+        
+              </li>
+            )}
+          </ul> 
+        </> :
+        <p>タスクを追加してください</p>
+      }
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={
+               ((event) => setInput(event.target.value))
+              }/>
+        <button type="submit">AddTasks</button>
+      </form>
     </>
   )
 }
